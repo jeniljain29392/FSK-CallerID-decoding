3657ced3e5f110e4704b25ee8c5ba181d580349d
jeniljain29392 <jeniljain29392@gmail.com>
2016-02-04 11:44:31 -0700

    Update ciddeco.c

--- ciddeco.c
+++ ciddeco.c
@@ -1,7 +1,6 @@
 /**@file ciddeco.c
  *	
  * @author Jenil Jain
- * @copyright Copyright 2015 Insensi Inc. All rights reserved.
  * This project is released under the GNU Public License.
  *
  * @brief Decodes CallerID message
